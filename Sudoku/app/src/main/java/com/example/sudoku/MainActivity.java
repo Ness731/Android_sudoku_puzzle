ca0bc6bb9c1b153525a57d0695fd2989e866c74d
JinJuJeong <wjdwlswn731@gmail.com>
2022-11-11 23:33:54 +0900

    :bug: Fix a bug of MainActivity

--- Sudoku/app/src/main/java/com/example/sudoku/MainActivity.java
+++ Sudoku/app/src/main/java/com/example/sudoku/MainActivity.java
@@ -8,43 +8,39 @@ import android.widget.TableLayout;
 import android.widget.TableRow;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.sudoku);
+        initContent();
+    }
 
-        // 1. Table Layout 생성
-        TableLayout table = (TableLayout)findViewById(R.id.tableLayout);
-        TableRow tableRow = new TableRow(this);
-        table.addView(tableRow);
+    private void initContent() {
+        final TableLayout tableLayout = (TableLayout) findViewById(R.id.tableLayout);
+        // 버튼이 화면을 넘어가지 않도록 자동 크기 조절
+        tableLayout.setShrinkAllColumns(true);
 
-        // 2. Button 생성
-        Button[][] buttons = new Button[9][9];
-        int left= 1;
-        int top = 1;
-        int right= 1;
-        int bottom = 1;
+        Button buttons[][] = new Button[9][9];
+        for (int i = 0; i < 9; i++) {
+            final TableRow tableRow = new TableRow(this);
+            tableRow.setLayoutParams(new TableLayout.LayoutParams(
+                    TableLayout.LayoutParams.WRAP_CONTENT,
+                    TableLayout.LayoutParams.WRAP_CONTENT)
+            );
+            for (int j = 0; j < 9; j++) {
 
-        for(int i=0; i<9; i++) {
-            for(int j=0; j<9; j++) {
-                buttons[i][j] = new Button(this);
-                tableRow.addView(buttons[i][j]);
-                TableRow.LayoutParams layoutParams =
-                        new TableRow.LayoutParams(
-                                TableRow.LayoutParams.WRAP_CONTENT,
-                                TableRow.LayoutParams.WRAP_CONTENT,
-                                1.0f
-                        );
-                layoutParams.setMargins(left, top, right, bottom);
-                buttons[i][j].setLayoutParams(layoutParams);
-                tableRow.addView(buttons[i][j]);
-                // 3. Button 위의 숫자 생성
                 BoardGenerator board = new BoardGenerator();
-                int number = board.get(i, j);
+                buttons[i][j] = new Button(this);
+                buttons[i][j].setLayoutParams(new TableRow.LayoutParams(
+                        TableRow.LayoutParams.WRAP_CONTENT,
+                        TableRow.LayoutParams.WRAP_CONTENT)
+                );
+                // board.get(i, j)는 int를 반환하고 setText는 String을 받기 때문에 String으로 형변환 필요
+                String number = Integer.toString(board.get(i, j));
                 buttons[i][j].setText(number);
+                tableRow.addView(buttons[i][j]);
             }
+            tableLayout.addView(tableRow);
         }
-
     }
 }
