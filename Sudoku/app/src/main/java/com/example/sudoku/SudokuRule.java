b980a0d15a3e0a54bce67bda838803f8e7c210d6
JinJuJeong <wjdwlswn731@gmail.com>
2022-11-21 14:05:20 +0900

    :sparkles: Add Game Rule Checker

--- Sudoku/app/src/main/java/com/example/sudoku/SudokuRule.java
+++ Sudoku/app/src/main/java/com/example/sudoku/SudokuRule.java
@@ -2,38 +2,32 @@ package com.example.sudoku;
 
 import java.util.*;
 
-public class Sudoku {
+public class SudokuRule {
     int[][] board;
     int[][] groups = new int[][]{{0, 1, 2}, {3, 4, 5}, {6, 7, 8}};
 
-    public Sudoku(int[][] board) {
+    public SudokuRule(int[][] board) {
         this.board = board;
     }
 
-    public boolean check() {
-        String status = "";
+    public boolean checkAll() {
         for (int row = 0; row < board.length; row++) { // 행
             for (int col = 0; col < board.length; col++) { // 열
                 int code = isValid(row, col);
                 if (code != -1) {
-                    switch (code) {
-                        case 1:
-                            status = "수평 중복";
-                            break;
-                        case 2:
-                            status = "수직 중복";
-                            break;
-                        case 3:
-                            status = "3x3 중복";
-                            break;
-                    }
-                    System.out.println("유효하지 않은 보드 : " + status);
-                    System.out.println("좌표: " + row + ", " + col + " | 값 : " + board[row][col]);
                     return false;
                 }
             }
         }
-        System.out.println("유효한 보드");
+        return true;
+    }
+
+    //좌표 한 개를 입력받아 해당 숫자의 유효성을 검사하는 메소드
+    public boolean check(int row, int col) {
+        int code = isValid(row, col);
+        if (code != -1) {
+            return false;
+        }
         return true;
     }
 
@@ -61,10 +55,12 @@ public class Sudoku {
 
         for (int n = 0; n < rowField.length; n++) {
             for (int m = 0; m < colField.length; m++) {
-                if (!(rowField[n] == row && colField[m] == col) &&
-                        board[rowField[n]][colField[m]] == board[row][col]){ //입력받은 좌표가 아니면서 숫자가 겹친다면
-                    System.out.println();
-                    return false;
+                if (board[row][col] != 0) {
+                    if (!(rowField[n] == row && colField[m] == col) &&
+                            board[rowField[n]][colField[m]] == board[row][col]) { //입력받은 좌표가 아니면서 숫자가 겹친다면
+                        System.out.println();
+                        return false;
+                    }
                 }
             }
         }
@@ -74,16 +70,20 @@ public class Sudoku {
 
     public boolean horizontalCheck(int row, int col) {
         for (int i = 0; i < board.length - 1; i++) {
-            if (i != col && board[row][i] == board[row][col]) //입력받은 좌표가 아니면서 숫자가 겹친다면
-                return false;
+            if (board[row][col] != 0) {
+                if (i != col && board[row][i] == board[row][col]) //입력받은 좌표가 아니면서 숫자가 겹친다면
+                    return false;
+            }
         }
         return true;
     }
 
     public boolean verticalCheck(int row, int col) {
         for (int i = 0; i < board.length - 1; i++) {
-            if (i != row && board[i][col] == board[row][col]) //입력받은 좌표가 아니면서 숫자가 겹친다면
-                return false;
+            if (board[row][col] != 0) {
+                if (i != row && board[i][col] == board[row][col]) //입력받은 좌표가 아니면서 숫자가 겹친다면
+                    return false;
+            }
         }
         return true;
     }
@@ -121,20 +121,21 @@ public class Sudoku {
         }
     }
 
-    public void setAllEmptyButton() {
+    public ArrayList<int[]> setAllEmptyButton() {
+        ArrayList<int[]> emptyButtons = new ArrayList<>();
         for (int[] rows : groups) {
             for (int[] cols : groups) {
-                setEmptyButton(rows, cols);
+                emptyButtons.addAll(setEmptyButton(rows, cols)); // 전체 보드에 대한 빈 블록 리스트를 구성
             }
-            System.out.println("---------------");
         }
+        return emptyButtons;
     }
 
-    private void setEmptyButton(int[] rows, int[] cols) {
+    private ArrayList<int[]> setEmptyButton(int[] rows, int[] cols) {
         ArrayList<int[]> elements = new ArrayList<>();
+        ArrayList<int[]> emptyButtons = new ArrayList<>();
         Random rand = new Random();
         int randomCount = rand.nextInt(5) + 2; // 빈 블록 개수는 2~6개
-        int[] randomIndex;
         int emptyRow, emptyCol;
         System.out.print("rows : ");
         print1dArr(rows);
@@ -155,40 +156,26 @@ public class Sudoku {
         // 빈 블록 좌표를 랜덤하게 선정
         for (int i = 0; i < randomCount; i++) {
             int idx = rand.nextInt(elements.size());
-            randomIndex = elements.get(idx);
+            emptyButtons.add(elements.get(idx));
             elements.remove(idx);
-            System.out.println(i + "번째 빈 블록 : (" + randomIndex[0] + ", " + randomIndex[1] + ")");
         }
-        System.out.println();
+        return emptyButtons;
     }
-
-    /* [빈 블록 생성 과정]
-    1. 랜덤하게 선정된 n개의 빈 블록 좌표를 받는다.
-    2. 해당 좌표의 버튼을 받아온다.
-    3. 해당 버튼 위에 빈 버튼을 겹치게 생성한다. -> 아마 레이아웃이 필요할듯?
-
-    ** 베이스 보드에 있는 버튼들은 클릭 비허용으로 바꿔야함!! **
-
-    [빈 블록 속성]
-    1. 클릭 허용 -> 온클릭 리스너로 다이어그램 띄우기
-    2. 다이어그램에서 선택된 숫자를 받아서 블록의 text로 설정하기
-
-    하 숫자 여러 개 넣는 건 또 어떻게 해야돼 개어렵네
-    */
-
+/*
     public static void main(String[] args) {
         BoardGenerator bg = new BoardGenerator();
         int[][] board = bg.get();
-        Sudoku test = new Sudoku(board);
+        SudokuRule test = new SudokuRule(board);
         test.printBoard();
-        test.check();
+        test.checkAll();
 
         int[][] board2 = bg.get();
         board2[0][2] = 5;
         board2[1][2] = 5;
         board2[2][2] = 5;
-        Sudoku test2 = new Sudoku(board2);
+        SudokuRule test2 = new SudokuRule(board2);
         test2.printBoard();
-        test2.check();
+        test2.checkAll();
     }
+ */
 }
